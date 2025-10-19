47212d02527e591b8b8f18504cba35f1409abef4
Nischal Keshav <nischalkeshav.official@gmail.com>
2025-10-19 07:58:54 -0500

    Revert "undo command nvim"
    This reverts commit 06f6d794a929250edb5fa8290a3aed93f797325d.
    
    Reverting to stable version

--- init.lua
+++ init.lua
@@ -1,5 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
--- My changesi
-vim.keymap.set("i", "<C-z>", "<C-o>u'", { noremap = true, silent = true }) -- Undo to command z or <D-z><D-:c>
