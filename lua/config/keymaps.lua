41dfd221b6a9c1f6a2de4718a87c74c12733d337
Nischal Keshav <nischalkeshav.official@gmail.com>
2025-10-19 08:44:34 -0500

    reverse slash now navigates to the file explorer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,21 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+--
+--
+--
+--
+--
+local map = vim.keymap.set
+
+-- Custom keymaps can be added here
+-- This keymap will toggle the file explorer
+map("n", "\\", function()
+  local explorer_pickers = Snacks.picker.get({ source = "explorer" })
+  if #explorer_pickers == 0 then
+    Snacks.picker.explorer()
+  else
+    explorer_pickers[1]:focus()
+  end
+end, { desc = "File Explorer" })
