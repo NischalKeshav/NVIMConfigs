814d137118be80ecec429c1a16d223aaf771f4eb
Nischal Keshav <nischalkeshav.official@gmail.com>
2025-11-20 14:36:27 -0600

    Update toggleterm.lua

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -12,6 +12,7 @@ return {
       -- Example keymaps for multiple terminals
       vim.keymap.set("n", "<leader>t1", ":ToggleTerm 1 direction=float<CR>", { desc = "Terminal 1" })
       vim.keymap.set("n", "<leader>t2", ":ToggleTerm 2 direction=float<CR>", { desc = "Terminal 2" })
+      vim.keymap.set("n", "<leader>t3", ":ToggleTerm 3 direction=float<CR>", { desc = "Terminal 2" })
       vim.keymap.set("n", "<leader>tv", ":ToggleTerm direction=vertical<CR>", { desc = "Vertical Terminal" })
       vim.keymap.set("n", "<leader>th", ":ToggleTerm direction=horizontal<CR>", { desc = "Horizontal Terminal" })
     end,
